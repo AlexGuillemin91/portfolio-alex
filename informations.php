b79a5e8925e30b92b98a32f47b9cc00e9b2e83c9
Alex <alex.guillemin@edu.devinci.fr>
2021-02-13 16:10:36 +0100

    page infos en lorem

---
+++ informations.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Template Name: Informations
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+		<section>
+			<article class="about-me">
+				<h3>about me</h3>
+				<div class="infos">
+					<div>
+						<h4>formations</h4>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi, vel facilis iure minima provident doloremque rem distinctio obcaecati sunt tempore, quos repudiandae. Explicabo, reiciendis deserunt! Perspiciatis fuga voluptas enim!</p>
+					</div>
+					<div>
+						<h4>experiences</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet voluptate omnis esse ipsum, laudantium ullam earum asperiores reprehenderit animi voluptatem consequatur ipsa mollitia! Commodi accusamus reprehenderit in, fugit non reiciendis?</p>
+					</div>
+					<div>
+						<h4>loisirs</h4>
+						<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo ipsa sint libero vitae nemo officiis, esse odio itaque magni placeat quis nesciunt dolorem culpa facere repellendus porro perferendis repudiandae autem.</p>
+					</div>
+				</div>
+			</article>
+			<hr>
+			<article class="skills">
+				<h3>skills</h3>
+				<div class="list-skills">
+					<div>
+						<img src="../wp-content/themes/portfolio-alex/img/html.svg">
+						<h4>html5</h4>
+					</div>
+					<div>
+						<img src="../wp-content/themes/portfolio-alex/img/css.svg">
+						<h4>css3</h4>
+					</div>
+					<div>
+						<img src="../wp-content/themes/portfolio-alex/img/sass.svg">
+						<h4>sass</h4>
+					</div>
+					<div>
+						<img src="../wp-content/themes/portfolio-alex/img/js.svg">
+						<h4>javascript</h4>
+					</div>
+					<div>
+						<img src="../wp-content/themes/portfolio-alex/img/react.svg">
+						<h4>react</h4>
+					</div>
+					<div>
+						<img src="../wp-content/themes/portfolio-alex/img/git.svg">
+						<h4>git</h4>
+					</div>
+					<div>
+						<img src="../wp-content/themes/portfolio-alex/img/mysql.svg">
+						<h4>mysql</h4>
+					</div>
+					<div>
+						<img src="../wp-content/themes/portfolio-alex/img/mongodb.svg">
+						<h4>mongodb</h4>
+					</div>
+				</div>
+			</article>
+		</section>
+	</main>
+</body>
